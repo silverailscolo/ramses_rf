82a83f7ce083e8f1853c37f2674d863be46433c2
Egbert Broerse <dcc2@ebroerse.nl>
2025-03-21 23:16:31 +0100

    add DevType.FAN code 2210 RP

--- src/ramses_tx/ramses.py
+++ src/ramses_tx/ramses.py
@@ -1060,7 +1060,7 @@ _DEV_KLASSES_HVAC: dict[str, dict[Code, dict[VerbT, Any]]] = {
         Code._1470: {RP: {}},
         Code._1F09: {I_: {}, RP: {}},
         Code._1FC9: {W_: {}},
-        Code._2210: {I_: {}},
+        Code._2210: {I_: {}, RP: {}},
         Code._22F1: {RP: {}},
         Code._22F2: {I_: {}},
         Code._22F3: {},
