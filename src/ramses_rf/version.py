7203d2a58dbf20ca66e470abb60bac85b2b76923
Egbert Broerse <dcc2@ebroerse.nl>
2025-04-23 17:38:51 +0200

    update 2210 parser, v8

--- src/ramses_rf/version.py
+++ src/ramses_rf/version.py
@@ -1,4 +1,4 @@
 """RAMSES RF - a RAMSES-II protocol decoder & analyser (application layer)."""
 
-__version__ = "0.50.2-beta7"
+__version__ = "0.50.2-beta8"
 VERSION = __version__
--- src/ramses_tx/version.py
+++ src/ramses_tx/version.py
@@ -1,4 +1,4 @@
 """RAMSES RF - a RAMSES-II protocol decoder & analyser (transport layer)."""
 
-__version__ = "0.50.2-beta7"
+__version__ = "0.50.2-beta8"
 VERSION = __version__
