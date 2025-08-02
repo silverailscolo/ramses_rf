7a25c8c27d62320159b0c74b79ea5406a4737030
Egbert Broerse <dcc2@ebroerse.nl>
2025-08-02 22:11:59 +0200

    more _tx unit tests

--- tests/tests_tx/test_command.py
+++ tests/tests_tx/test_command.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-"""Test ramses_tx/command.py"""
+"""Unittests for ramses_tx/command.py"""
 
 import logging
-from typing import Final  # , NoReturn, TypeAlias, TypedDict
-
-#  from unittest.mock import patch
-import serial as ser  # type: ignore[import-untyped]
-from serial.tools.list_ports import comports  # type: ignore[import-untyped]
+from datetime import datetime as dt, timedelta as td
+from typing import Final
 
 from ramses_tx.command import Command
 from ramses_tx.const import SYS_MODE_MAP, ZON_MODE_MAP
@@ -18,15 +15,15 @@ _LOGGER = logging.getLogger(__name__)
 
 #######################################################################################
 
-#######################################################################################
-
-#######################################################################################
-
+# until an hour from now as "2025-08-02 14:00:00"
+_UNTIL = (dt.now().replace(minute=0, second=0, microsecond=0) + td(hours=2)).strftime(
+    "%Y-%m-%d %H:%M:%S"
+)
 
 TEST_COMMANDS: Final = [
-    " W --- 18:000730 12:123456 --:------ 1F41 006 00FF00FFFFFF",
-    " W --- 18:000730 12:123456 --:------ 2E04 008 00FFFFFFFFFFFF00",
-    " W --- 18:000730 12:123456 --:------ 2349 007 027FFF00FFFFFF",
+    " W --- 18:000730 12:123456 --:------ 1F41 006 00FF00FFFFFF",  # set_dhw_mode
+    " W --- 18:000730 12:123456 --:------ 2E04 008 00FFFFFFFFFFFF00",  # set_system_mode
+    " W --- 18:000730 12:123456 --:------ 2349 007 027FFF00FFFFFF",  # set_zone_mode
 ]
 
 
@@ -44,7 +41,7 @@ async def test_set_dhw_mode_follow() -> None:
 
 
 async def test_set_dhw_mode_follow_int() -> None:
-    """Test parameter checks"""
+    """Test parameter checks from int"""
     cmd = Command.set_dhw_mode(ctl_id="12:123456", mode=0)
     #  cls,
     #  ctl_id: DeviceIdT | str,
@@ -68,30 +65,75 @@ async def test_set_dhw_mode_follow_extra() -> None:
         assert False
 
 
+async def test_set_dhw_mode_untilduration() -> None:
+    """Test parameter checks extra"""
+    try:
+        _ = Command.set_dhw_mode(
+            ctl_id="12:123456",
+            mode="temporary_override",
+            active=True,
+            duration=3600,
+            until=_UNTIL,  # Invalid args: At least one of until or duration must be None
+        )
+    except CommandInvalid:
+        pass
+    else:
+        assert False
+
+
+async def test_set_system_mode_auto_none() -> None:
+    """Test parameter checks from int"""
+    cmd = Command.set_system_mode(ctl_id="12:123456", system_mode=None)
+
+    assert str(cmd) == TEST_COMMANDS[1]
+
+
 async def test_set_system_mode_auto() -> None:
     """Test parameter checks"""
     cmd = Command.set_system_mode(ctl_id="12:123456", system_mode=SYS_MODE_MAP["auto"])
-    #  cls,
-    #  ctl_id: DeviceIdT | str,
-    #  mode: int | str | None = None,
-    #  active: bool | None = None,
-    #  until: dt | str | None = None,
-    #  duration: int | None = None,
+    # cls,
+    # ctl_id: DeviceIdT | str,
+    # system_mode: int | str | None,
+    # *,
+    # until: dt | str | None = None,
 
     assert str(cmd) == TEST_COMMANDS[1]
 
 
 async def test_set_system_mode_auto_int() -> None:
-    """Test parameter checks"""
+    """Test parameter checks from int"""
     cmd = Command.set_system_mode(ctl_id="12:123456", system_mode=0)
-    #  cls,
-    #  ctl_id: DeviceIdT | str,
-    #  mode: int | str | None = None,
-    #  active: bool | None = None,
-    #  until: dt | str | None = None,
-    #  duration: int | None = None,
 
     assert str(cmd) == TEST_COMMANDS[1]
+
+
+async def test_set_system_mode_heatoff() -> None:
+    """Test parameter checks mode 1"""
+
+    try:
+        _ = Command.set_system_mode(
+            ctl_id="12:123456",
+            system_mode=SYS_MODE_MAP.HEAT_OFF,  # until should be None
+            until="456789566",
+        )
+    except CommandInvalid:
+        pass
+    else:
+        assert False
+
+
+async def test_set_zone_mode_noargs() -> None:
+    """Test parameter checks extra"""
+
+    try:
+        _ = Command.set_zone_mode(
+            ctl_id="12:123456",
+            zone_idx=4,
+        )
+    except CommandInvalid:
+        pass
+    else:
+        assert False
 
 
 async def test_set_zone_mode_follow() -> None:
@@ -103,6 +145,7 @@ async def test_set_zone_mode_follow() -> None:
     #  ctl_id: DeviceIdT | str,
     #  mode: int | str | None = None,
     #  zone_idx: _ZoneIdxT,
+    #  *
     #  active: bool | None = None,
     #  until: dt | str | None = None,
     #  duration: int | None = None,
@@ -111,7 +154,7 @@ async def test_set_zone_mode_follow() -> None:
 
 
 async def test_set_zone_mode_follow_extra() -> None:
-    """Test parameter checks"""
+    """Test parameter checks extra"""
 
     try:
         _ = Command.set_zone_mode(
