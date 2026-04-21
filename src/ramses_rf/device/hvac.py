9023d10c81bcf805a9e719ba6ed29b3d9e685a49
Egbert Broerse <dcc2@ebroerse.nl>
2026-04-21 09:53:06 +0200

    invert if discov condition

--- src/ramses_rf/device/hvac.py
+++ src/ramses_rf/device/hvac.py
@@ -276,7 +276,7 @@ class FilterChange(DeviceHvac):  # FAN: 10D0
             RQ, self.id, Code._10D0, PayloadT("00")
         )
 
-        if not self._gwy.config.disable_discovery:  # discovery will poll for filter
+        if self._gwy.config.disable_discovery:  # discovery will poll for filter
             try:
                 asyncio.get_running_loop().call_soon(self.start_poller)
             except RuntimeError:
