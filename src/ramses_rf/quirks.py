555a10ea2547e168850c53968c2e685b284fe987
Egbert Broerse <dcc2@ebroerse.nl>
2026-07-04 22:07:37 +0200

    Tweak 31da quirk (#778)
    * add Ventura 31DA log bypass closed, add test, quirk 3 and
    * leave None alone
    * add test_quirks for orcon
    * test 31da quirk
    
    ---------
    
    Co-authored-by: PWhite-Eng <75939018+PWhite-Eng@users.noreply.github.com>

--- src/ramses_rf/quirks.py
+++ src/ramses_rf/quirks.py
@@ -192,11 +192,9 @@ def apply_hvac_quirks(
     if msg_code == "31DA" and "bypass_position" in mutated:
         if mutated["bypass_position"] == 0.0:
             if (
-                current_state.bypass_position is not None
-                and (
-                    current_state.bypass_position != 0.0
-                    or current_state.bypass_mode is not None
-                )  # from 22F7
+                current_state.bypass_position != 0.0
+                and current_state.bypass_mode is not None
+                # would be set from 22F7
             ):
                 mutated["bypass_position"] = current_state.bypass_position
 
